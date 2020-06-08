ca4a71fde97c5ed5d873e1fda7187a5691575f98
Kerri-Ann <kaybee1jam@yahoo.com>
2020-06-08 18:32:12 -0400

    connecting to redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/app.js",
+	entry: "./src/playground/test.js",
 	output: {
 		path: path.join(__dirname, "public"),
 		filename: "bundle.js"
