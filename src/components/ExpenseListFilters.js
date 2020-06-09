8a834d4a60d2ed5a06c1ef5f74d88bee8fb5ca95
Kerri-Ann <kaybee1jam@yahoo.com>
2020-06-08 20:10:06 -0400

    completed base sorting functionality

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { connect } from "react-redux";
+import { setTextFilter, sortByAmout, sortByDate } from "../actions/filters";
 
-const ExpenseListFilters = () => (
+const ExpenseListFilters = (props) => (
 	<div>
-		<input type="text" />
+		<input
+			type="text"
+			value={props.filters.text}
+			onChange={(e) => {
+				props.dispatch(setTextFilter(e.target.value));
+			}}
+		/>
+		<select
+			value={props.filters.sortBy}
+			onChange={(e) => {
+				if (e.target.value == "amount") {
+					props.dispatch(sortByAmout());
+				} else if (e.target.value == "date") {
+					props.dispatch(sortByDate());
+				}
+			}}
+		>
+			<option value="date">Date</option>
+			<option value="amount">Amount</option>
+		</select>
 	</div>
 );
 
-export default ExpenseListFilters;
+const mapStateToProps = (state) => {
+	return {
+		filters: state.filters
+	}
+}
+
+export default connect(mapStateToProps)(ExpenseListFilters);
+
