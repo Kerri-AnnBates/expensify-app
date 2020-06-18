54ca232c637c7ffbf65b007e417b3b746436fd93
Kerri-Ann <kaybee1jam@yahoo.com>
2020-06-18 13:55:34 -0400

    write tests to test props and function calls

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -6,4 +6,4 @@ import expenses from "../fixtures/expenses";
 test("should render expense list item", () => {
 	const wrapper = shallow(<ExpenseListItem {...expenses[1]} />);
 	expect(wrapper).toMatchSnapshot();
-})
+});
