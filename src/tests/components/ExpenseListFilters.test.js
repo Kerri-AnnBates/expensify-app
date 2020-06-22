17c6da3a51427545eae122fddcc7f76f51cc3575
Kerri-Ann <kaybee1jam@yahoo.com>
2020-06-22 13:47:11 -0400

    finalized tests for filters

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
+import moment from "moment";
 import { ExpenseListFilters } from "../../components/ExpenseListFilters";
 import { filters, altFilters } from "../fixtures/filters";
 
@@ -31,5 +32,53 @@ test("should render ExpenseListFiltersCorrectly with alt data correctly", () => 
 	wrapper.setProps({
 		filters: altFilters
 	});
+
 	expect(wrapper).toMatchSnapshot();
+});
+
+// should handle text change
+test("should handle text change", () => {
+	const text = "new text";
+
+	wrapper.find("input").simulate("change", {
+		target: { value: text }
+	});
+
+	expect(setTextFilter).toHaveBeenLastCalledWith(text);
+});
+
+// should sort by date
+test("should sort by date", () => {
+	wrapper.find("select").simulate("change", {
+		target: { value: "date" }
+	});
+
+	expect(sortByDate).toHaveBeenCalled();
+});
+
+// should sort by amount
+test("should sort by amount", () => {
+	wrapper.find("select").simulate("change", {
+		target: { value: "amount" }
+	});
+
+	expect(sortByAmount).toHaveBeenCalled();
+});
+
+// should handle date changes
+test("should handle date changes", () => {
+	const startDate = moment(0);
+	const endDate = moment(0).add(3, "days");
+
+	wrapper.find("DateRangePicker").prop("onDatesChange")({ startDate, endDate });
+
+	expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+	expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+});
+
+// should handle date focus changes
+test("should handle date focus changes", () => {
+	wrapper.find("DateRangePicker").prop("onFocusChange")(true);
+
+	expect(wrapper.state("calendarFocused")).toBe(true);
 });
