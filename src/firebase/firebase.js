2d7feb24b9b7a1949eba6f5e93572fb531b5f7f6
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-02 14:15:49 -0400

    setup environment variables for dev/testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCiq_P4lU8zPhbuf30GhotcXrWSjleFspk",
-	authDomain: "expensify-app-af7b3.firebaseapp.com",
-	databaseURL: "https://expensify-app-af7b3.firebaseio.com",
-	projectId: "expensify-app-af7b3",
-	storageBucket: "expensify-app-af7b3.appspot.com",
-	messagingSenderId: "522969163719",
-	appId: "1:522969163719:web:39e2cec81702df76aa5be2"
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
