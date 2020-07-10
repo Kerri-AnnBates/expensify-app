da172aaa85c3ba61c4cf45ee449eafc1809a969b
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-10 14:17:16 -0400

    basic setup of authentication with firebase

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
+import { startLogin } from "../actions/auth";
 
-const LoginPage = () => (
+export const LoginPage = ({ startLogin }) => (
 	<div>
-		<form>
-			<button>Login</button>
-		</form>
+		<button onClick={startLogin}>Login</button>
 	</div>
 )
 
-export default LoginPage;
+const mapDispatchToProps = (dispatch) => ({
+	startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
 
--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
-import LoginPage from "../../components/LoginPage";
+import { LoginPage } from "../../components/LoginPage";
 
+let wrapper, startLogin;
+
+beforeEach(() => {
+	startLogin = jest.fn();
+	wrapper = shallow(<LoginPage startLogin={startLogin} />);
+})
 test("should render login page correctly", () => {
-	const wrapper = shallow(<LoginPage />);
 	expect(wrapper).toMatchSnapshot();
+});
+
+test("should call startLogin on button click", () => {
+	wrapper.find("button").simulate("click");
+	expect(startLogin).toHaveBeenCalled();
 });
