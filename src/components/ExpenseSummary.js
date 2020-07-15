50c08f3773d759629a5ef380ce846958a113cb2f
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-15 09:21:32 -0400

    styled header and header summary

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import getExpenseTotal from "../selectors/expense-total";
 import selectExpenses from "../selectors/expenses";
 import numeral from "numeral";
 
 export const ExpenseSummary = ({ expenseCount, expensesTotal }) => (
-	<div>
-		<p>Viewing {expenseCount} {expenseCount === 1 ? `expense` : `expenses`} totaling {numeral(expensesTotal / 100).format("$0,0.00")}</p>
+	<div className="page-header">
+		<div className="content-container">
+			<h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseCount === 1 ? `expense` : `expenses`} totaling <span>{numeral(expensesTotal / 100).format("$0,0.00")}</span></h1>
+			<div className="page-header__actions">
+				<Link to="/create" className="button">Add Expense</Link>
+			</div>
+		</div>
 	</div>
 );
 
