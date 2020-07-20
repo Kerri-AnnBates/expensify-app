05fa10eda5a5a6921e24f7d359b1191581618c0d
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-20 14:07:59 -0400

    Styled modal

--- src/components/RemoveModal.js
+++ src/components/RemoveModal.js
@@ -2,14 +2,22 @@ import React from "react";
 import Modal from 'react-modal';
 
 const RemoveModal = (props) => (
-	<Modal isOpen={props.isOpen}>
-		<h3>Remove Expense</h3>
-		<p>Are you sure you want to remove the {props.expense.description} expense?</p>
-		<button
-			className="button"
-			onClick={props.onRemove}
-		>Yes</button>
-		<button className="button button--secondary" onClick={props.handleCloseModal} >No</button>
+	<Modal isOpen={props.isOpen} className="modal">
+		<div className="modal__title">
+			<div className="content-container">
+				<h3>Remove Expense</h3>
+			</div>
+		</div>
+		<div className="modal__body">
+			<div className="content-container">
+				<p>Are you sure you want to remove the <span>{props.expense.description}</span> expense?</p>
+				<button
+					className="button"
+					onClick={props.onRemove}
+				>Yes</button>
+				<button className="button button--secondary" onClick={props.handleCloseModal} >No</button>
+			</div>
+		</div>
 	</Modal>
 );
 
