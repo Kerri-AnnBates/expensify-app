584966eda08339283c4d633c50e40c141b696fe3
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-21 12:18:20 -0400

    add styles to summary subtitle

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -9,9 +9,9 @@ export const ExpenseSummary = ({ expenseCount, expensesTotal, allExpensesCount }
 	<div className="page-header">
 		<div className="content-container">
 			<h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseCount === 1 ? `expense` : `expenses`} totaling <span>{numeral(expensesTotal / 100).format("$0,0.00")}</span></h1>
-			<p>{(expenseCount === allExpensesCount) ?
+			<p className="page-header__subtitle">{(expenseCount === allExpensesCount) ?
 				`Showing all expenses, no filters in place` :
-				`Not showing ${allExpensesCount - expenseCount} expense because of filters. Remove date filters to see all expenses`}</p>
+				`Not showing ${allExpensesCount - expenseCount} expense(s) because of filters. Remove date filters to see all expenses`}</p>
 			<div className="page-header__actions">
 				<Link to="/create" className="button">Add Expense</Link>
 			</div>
