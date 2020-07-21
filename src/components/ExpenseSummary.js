92e29da9cc964c6408825f5e8bc11102e9218325
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-21 12:01:04 -0400

    added summary for expenses not visible

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -5,10 +5,13 @@ import getExpenseTotal from "../selectors/expense-total";
 import selectExpenses from "../selectors/expenses";
 import numeral from "numeral";
 
-export const ExpenseSummary = ({ expenseCount, expensesTotal }) => (
+export const ExpenseSummary = ({ expenseCount, expensesTotal, allExpensesCount }) => (
 	<div className="page-header">
 		<div className="content-container">
 			<h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseCount === 1 ? `expense` : `expenses`} totaling <span>{numeral(expensesTotal / 100).format("$0,0.00")}</span></h1>
+			<p>{(expenseCount === allExpensesCount) ?
+				`Showing all expenses, no filters in place` :
+				`Not showing ${allExpensesCount - expenseCount} expense because of filters. Remove date filters to see all expenses`}</p>
 			<div className="page-header__actions">
 				<Link to="/create" className="button">Add Expense</Link>
 			</div>
@@ -21,7 +24,8 @@ const mapStateToProps = (state) => {
 
 	return {
 		expenseCount: expenses.length,
-		expensesTotal: getExpenseTotal(expenses)
+		expensesTotal: getExpenseTotal(expenses),
+		allExpensesCount: state.expenses.length
 	}
 }
 
