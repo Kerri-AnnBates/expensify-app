964d5120496957aecb20ea3ed39e0bb095bf0233
Kerri-Ann <kaybee1jam@yahoo.com>
2020-07-22 16:34:30 -0400

    add fb auth option

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,8 +14,9 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
 
-export { firebase, googleAuthProvider, database as default };
+export { firebase, googleAuthProvider, facebookAuthProvider, database as default };
 
 // database.ref("expenses").on("value", (snapshot) => {
 // 	const expenses = [];
